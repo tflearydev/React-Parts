61cee068d4a9d9e93683a16ca8ac3b0fa3b14f4e
Tray Fleary <tflearydev@gmail.com>
2020-01-21 13:00:01 -0500

    updated components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
@@ -8,15 +8,23 @@ import { Confirmation } from './components/Confirmation'
 // import pizzas from '.data';
 
 function App() {
+    const [ordered]
+
   return (
-    <>
+    
     <Confirmation />
     <Container>
       <Row>
-        <p>Hello There</p>
+        {pizzas.map(data => (
+          <Col xs={3} className="mb-5" key={`${data.id}`}>
+            <PizzaCard data={data} />
+          </Col>
+        )
+        )}
+        
       </Row>
     </Container>
-    </>
+    
   );
 }
 
--- my-app/src/components/PizzaCard.jsx
+++ my-app/src/components/PizzaCard.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+import { Card, Badge, Button} from 'react-bootstrap;'
+
+export function PizzaCard({ data, setOrdered }) {
+    return (
+        <Card className="h-100 shadow-sm bg-white rounded">
+            <Card.Img variant="top" src={data.image} />
+            <Card.Body className="d-flex flex-solumn">
+                <div className="d-flex mb-2 justify-content-between">
+                    <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
+
+                </div>
+
+            </Card.Body>
+        </Card>
+    )
+}
