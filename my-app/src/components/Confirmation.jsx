4c988779ec585a747fe7343d7f50211d07213e35
Tray Fleary <tflearydev@gmail.com>
2020-01-21 14:09:15 -0500

    updating content

--- my-app/src/components/Confirmation.jsx
+++ my-app/src/components/Confirmation.jsx
@@ -6,12 +6,12 @@ export function Confirmation({ toggle }) {
     return (
         <Toast onClose={() => toggle(false)}>
             <Toast.Header>
-                <strong className="mr-auto">Your Order Is In The Over</strong>
-                <small className="mr-auto">just now</small>
+                <strong className="mr-auto">Your Part's Order Is Being Processed</strong>
+                <small>just now</small>
             </Toast.Header>
             <Toast.Body>
                 Your delicious pizza will be with you in 30 minutes!
             </Toast.Body>
         </Toast>
-    )
+    );
 }
