47259985817300ea9968a91f5a4127a8e1e07cf5
Tray Fleary <tflearydev@gmail.com>
2020-01-21 16:03:44 -0500

    updating data.json

--- my-app/src/components/PizzaCard.jsx
+++ my-app/src/components/PizzaCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Badge, Button} from 'react-bootstrap'
 
+
 export function PizzaCard({ data, setOrdered }) {
     return (
         <Card className="h-100 shadow-sm bg-white rounded">
