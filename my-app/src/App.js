ae0979b9bbca46b9e4d083a0b1e07069b67d8ac2
Tray Fleary <tflearydev@gmail.com>
2020-01-22 12:15:35 -0500

    updated carousel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 
 import { Container, Row, Col } from 'react-bootstrap'; 
 import Carousel from 'react-bootstrap/Carousel'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
 import { PizzaCard } from './components/PizzaCard';
 import { Confirmation } from './components/Confirmation';
 import pizzas from './data';
@@ -23,11 +23,10 @@ function App() {
 
   return (
 <>
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
+<Navbar bg="dark" variant="dark" sticky="top">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
@@ -38,14 +37,11 @@ function App() {
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
 
 
 <Carousel>
@@ -56,8 +52,8 @@ function App() {
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3 className="caro-text">Your Parts, Exactly When You Need Them!</h3>
-      <p className="caro-text">New & Improved Search and Shipping</p>
+      <h3 className="caro-text">Your Parts, Exactly <br></br>When You Need Them!</h3>
+      <p>New & Improved Search and Shipping</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -68,8 +64,8 @@ function App() {
     />
 
     <Carousel.Caption>
-    <h3 className="caro-text">Your Parts, Exactly When You Need Them!</h3>
-      <p className="caro-text">New & Improved Search and Shipping</p>
+    <h3 className="caro-text">Your Parts, Exactly <br></br>When You Need Them!</h3>
+      <p>New & Improved Search and Shipping</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -80,8 +76,8 @@ function App() {
     />
 
     <Carousel.Caption>
-    <h3 className="caro-text">Your Parts, Exactly When You Need Them!</h3>
-      <p className="caro-text">New & Improved Search and Shipping</p>
+    <h3 className="caro-text">Your Parts, Exactly <br></br>When You Need Them!</h3>
+      <p className="caro-small">New & Improved Search and Shipping</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
