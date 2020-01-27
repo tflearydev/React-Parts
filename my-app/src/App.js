cf165ed603fbdcb2f3072d1512e02cb9089a936b
Tray Fleary <tflearydev@gmail.com>
2020-01-27 15:40:44 -0500

    testing react css transition group api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
--- my-app/src/components/Carousel.jsx
+++ my-app/src/components/Carousel.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import Carousel from "react-bootstrap/Carousel";
 import trucks from "../trucks.jpg";
 
 function Caro() {
-  
-        return (
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100 caro" src={trucks} alt="First slide" />
 
-            <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100 caro" src={trucks} alt="First slide" />
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
           <Carousel.Caption>
             <div className="gradient">
               <p className="caro-text">
@@ -17,34 +24,51 @@ function Caro() {
               <p className="caro-small">New & Improved Search and Shipping</p>
             </div>
           </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 caro" src={trucks} alt="Third slide" />
+        </ReactCSSTransitionGroup>
+      </Carousel.Item>
 
+      <Carousel.Item>
+        <img className="d-block w-100 caro" src={trucks} alt="Third slide" />
+
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
           <Carousel.Caption>
             <div className="gradient">
               <p className="caro-text">
-              Your Parts, Exactly When You Need Them!
+                Your Parts, Exactly When You Need Them!
               </p>
               <p className="caro-small">New & Improved Search and Shipping</p>
             </div>
           </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 caro" src={trucks} alt="Third slide" />
+        </ReactCSSTransitionGroup>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100 caro" src={trucks} alt="Third slide" />
 
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
           <Carousel.Caption>
             <div className="gradient">
               <p className="caro-text">
-              Your Parts, Exactly When You Need Them!
+                Your Parts, Exactly When You Need Them!
               </p>
               <p className="caro-small">New & Improved Search and Shipping</p>
             </div>
           </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-        )
-    }
-
+        </ReactCSSTransitionGroup>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 export default Caro;
