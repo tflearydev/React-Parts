858a6fd7415df2ce1c459d1f933429947a560908
Tray Fleary <tflearydev@gmail.com>
2020-02-10 15:36:19 -0500

    testing language change feature

---
+++ my-app/src/components/Drop.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Menu, Dropdown, Button, Icon, message  } from 'antd';
+// import './App.scss';
+
+// function Drop() {
+
+//   return (
+
+//   <div className="App">
+//     <Button type="primary">Button</Button>
+//   </div>
+//   )
+
+// };
+
+// export default Drop;
+
+
+function handleButtonClick(e) {
+  message.info('Click on left button.');
+  console.log('click left button', e);
+}
+
+function handleMenuClick(e) {
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1">
+      <Icon type="user" />
+      Spanish
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Icon type="user" />
+      German
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="user" />
+      Latin
+    </Menu.Item>
+  </Menu>
+);
+
+function Drop() {
+
+  return (
+  <div id="components-dropdown-demo-dropdown-button">
+    {/* <Dropdown.Button onClick={handleButtonClick} overlay={menu}>
+      Dropdown
+    </Dropdown.Button>
+    <Dropdown.Button overlay={menu} icon={<Icon type="user" />}>
+      Dropdown
+    </Dropdown.Button>
+    <Dropdown.Button onClick={handleButtonClick} overlay={menu} disabled>
+      Dropdown
+    </Dropdown.Button> */}
+    <Dropdown overlay={menu}>
+      <Button className="antbtn">
+        English <Icon type="down" />
+      </Button>
+    </Dropdown>
+  </div>
+);
+
+}
+
+export default Drop;
--- my-app/src/components/SimpleNav.jsx
+++ my-app/src/components/SimpleNav.jsx
@@ -8,43 +8,26 @@ import {
     NavDropdown
   } from "react-bootstrap";
   import trucklogo from "../trucklogo.png";
+  import  Drop  from '../components/Drop'
 
   function SimpleNav() {
 
 
       return (
-        <Navbar expand="lg" sticky="top" bg="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={trucklogo}
-            width="30"
-            height="30"
-            className="d-inline-block align-middle"
-          />{" "}
-          
-        </Navbar.Brand>
-        <span className="nav-text">U-Part</span>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <NavDropdown title="Parts" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Hood</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Windshields
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Fenders</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">More</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+           
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            
+          </Nav>
+          <Drop />
+      
       </Navbar>
       )
       };
